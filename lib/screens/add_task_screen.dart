7f84693955015bce2381f9167e3b70808d240304
Mantab Tadayon <mt.tadayon@gmail.com>
2020-01-31 16:16:47 +0100

    Refactoring AddTaskScreen to pop up just above the keyboard and be scrollable

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -3,52 +3,56 @@ import 'package:flutter/material.dart';
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFF626262),
+    return SingleChildScrollView(
       child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30.0),
-              topLeft: Radius.circular(30.0),
-            )),
-        child: Padding(
-          padding: EdgeInsets.all(40.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Add Task',
-                style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontSize: 30.0,
-                ),
-              ),
-              TextField(
-                autofocus: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  print(value);
-                },
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                width: double.infinity,
-                child: FlatButton(
-                  color: Colors.lightBlueAccent,
-                  onPressed: () {
-                    print('Hello');
-                  },
-                  disabledColor: Colors.lightBlueAccent,
-                  child: Text(
-                    'Add',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                    ),
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        color: Color(0xFF626262),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30.0),
+                topLeft: Radius.circular(30.0),
+              )),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Add Task',
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 30.0,
                   ),
                 ),
-              )
-            ],
+                TextField(
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    color: Colors.lightBlueAccent,
+                    onPressed: () {
+                      print('Hello');
+                    },
+                    disabledColor: Colors.lightBlueAccent,
+                    child: Text(
+                      'Add',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
