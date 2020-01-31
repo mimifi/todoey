d013f7948079c0a22ac9c647321c9c33e15937b2
Mantab Tadayon <mt.tadayon@gmail.com>
2020-01-31 17:34:15 +0100

    Manage to change the state of checkboxes and react of that in the UI

--- lib/Widgets/task_tile.dart
+++ lib/Widgets/task_tile.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'This is a new task.',
-        style: TextStyle(fontSize: 18.0),
+        style: TextStyle(
+          fontSize: 18.0,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
-      trailing: Checkbox(
-        onChanged: (bool value) {
-          print((value));
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
         },
-        value: false,
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckBox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
